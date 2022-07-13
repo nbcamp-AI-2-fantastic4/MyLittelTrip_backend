77f613b5eb3654a232810fbb5289a959ba84d181
mankic <miic0803@gmail.com>
2022-07-13 11:57:04 +0900

    추가 - review 어플리케이션 모델 생성 (Review, ReviewImage)

--- review/models.py
+++ review/models.py
@@ -1,3 +1,25 @@
+from distutils.command.upload import upload
 from django.db import models
 
-# Create your models here.
+from user.models import User
+from trip.models import Trip
+
+# 리뷰 모델
+class Review(models.Model):
+    user = models.ForeignKey(User, verbose_name='작성자', on_delete=models.SET_NULL, null=True)
+    trip = models.ForeignKey(Trip, verbose_name='여행 일정', on_delete=models.SET_NULL, null=True)
+    title = models.CharField('제목', max_length=50)
+    content = models.TextField('내용')
+    created_at = models.DateTimeField('등록일', auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s review - {self.id}"
+
+# 리뷰 이미지 모델
+class ReviewImage(models.Model):
+    review = models.ForeignKey(Review, verbose_name='리뷰', on_delete=models.CASCADE)
+    image = models.ImageField('이미지', upload_to='static/images/')
+    order = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.review.id}'s image - {self.order}"
