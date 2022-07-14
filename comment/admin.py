c4c23291ae199556cbaf4d73b70d820a2a08d2a1
J1NU2 <sdrtst@naver.com>
2022-07-14 14:32:01 +0900

    추가 - 댓글 및 좋아요 admin 커스터마이징

--- comment/admin.py
+++ comment/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
+
 from .models import Like, Comment, PostType
 
-admin.site.register(Like)
-admin.site.register(Comment)
-admin.site.register(PostType)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "posttype", "post_id"]
+    list_display_links = ["id", "post_id"]
+    list_filter = ["user", "posttype"]
+
+    search_fields = ["user__username", "posttype__typename", "post_id"]
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["id", "user", "posttype", "post_id", "comment", "rating", "created_at"]
+    list_display_links = ["id", "post_id", "comment"]
+    list_filter = ["user", "posttype"]
+
+    search_fields = ["user__username", "posttype__typename", "post_id", "rating"]
+
+class PostTypeAdmin(admin.ModelAdmin):
+    list_display = ["id", "typename"]
+    list_display_links = ["id", "typename"]
+
+    search_fields = ["typename"]
+
+admin.site.register(Like, LikeAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(PostType, PostTypeAdmin)
