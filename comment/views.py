b4f9188e8c2de5e4957c6aae17279b07fd7c1e3b
J1NU2 <sdrtst@naver.com>
2022-07-15 17:07:18 +0900

    추가 - 댓글 및 좋아요 API 초기

--- comment/views.py
+++ comment/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+
+from user.models import User
+
+from comment.models import PostType, Comment, Like
+from comment.serializers import CommentSerializer, LikeSerializer
+
+class CommentView(APIView):
+    # 댓글 조회 : /comment/<posttype_id>/<post_id>/
+    def get(self, request):
+        return Response({})
+
+    # 댓글 작성 : /comment/
+    def post(self, request):
+        return Response({})
+
+    # 댓글 수정 : /comment/<comment_id>
+    def put(self, request):
+        return Response({})
+
+    # 댓글 삭제 : /comment/<comment_id>
+    def delete(self, request):
+        return Response({})
+
+class LikeView(APIView):
+    # 좋아요 등록 : /like/
+    def post(self, request):
+        return Response({})
+
+    # 좋아요 취소 : /like/
+    def delete(self, request):
+        return Response({})
