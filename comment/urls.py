bd1f55a86385f983c2e925e6b8bd5aa4bd438a7e
J1NU2 <sdrtst@naver.com>
2022-07-28 22:01:26 +0900

    수정 - 댓글 URL name 추가

--- comment/urls.py
+++ comment/urls.py
@@ -3,7 +3,7 @@ from comment import views
 
 urlpatterns = [
     # comment/
-    path('', views.CommentView.as_view()),
-    path('<comment_id>/', views.CommentView.as_view()),
-    path('<posttype_id>/<post_id>/', views.CommentView.as_view())
+    path('', views.CommentView.as_view(), name="comment_post_view"),
+    path('<comment_id>/', views.CommentView.as_view(), name="comment_put_delete_view"),
+    path('<posttype_id>/<post_id>/', views.CommentView.as_view(), name="comment_get_view")
 ]
