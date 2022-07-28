8b0dcbf073db076d6f200c1fab589c192f0f1e69
mankic <miic0803@gmail.com>
2022-07-28 20:16:58 +0900

    수정 - url name 설정

--- review/urls.py
+++ review/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 # review/
 urlpatterns = [
-    path('', views.ReviewView.as_view()),
-    path('<review_id>/', views.ReviewDetailView.as_view()),
+    path('', views.ReviewView.as_view(), name='review_view'),
+    path('<review_id>/', views.ReviewDetailView.as_view(), name='review_detail_view'),
 ]
