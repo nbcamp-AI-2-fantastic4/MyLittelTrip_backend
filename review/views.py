d0555d8d0966211ac78cd02dde34ab7388b9f463
mankic <miic0803@gmail.com>
2022-08-03 03:56:14 +0900

    수정 - 리뷰 작성 페이지에서 로그인유저의 trip 데이터를 받아올 수 있게 view와 serializer 생성, immutable 에러 수정, user 임시값 넣어줌

--- review/views.py
+++ review/views.py
@@ -6,9 +6,9 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 from review.models import Review, ReviewImage
-from review.serializers import ReviewDetailSerializer, ReviewImageSerializer, ReviewSerializer
+from review.serializers import ReviewDetailSerializer, ReviewImageSerializer, ReviewSerializer, ReviewUserTripSerializer
 from user.models import User
-
+from trip.models import Trip
 
 # 리뷰 기능
 class ReviewView(APIView):
@@ -21,16 +21,18 @@ class ReviewView(APIView):
 
         reviews_serializer = ReviewSerializer(reviews, many=True).data
 
-        return Response(reviews_serializer, status=status.HTTP_200_OK)
+        return Response({"review_list":reviews_serializer}, status=status.HTTP_200_OK)
 
         
     # 리뷰 작성하기 : 인증된 회원만
     def post(self, request):
-        if not request.user.is_authenticated:
-            return Response({"message": "로그인해주세요"}, status=status.HTTP_401_UNAUTHORIZED)
-
+        # if not request.user.is_authenticated:
+        #     return Response({"message": "로그인해주세요"}, status=status.HTTP_401_UNAUTHORIZED)
+        # user_id = request.user.id
+        user_id = 1
+        setattr(request.data, '_mutable', True)
         trip_id = request.data.pop('trip_id')[0]
-        user_id = request.data.pop('user_id')[0]
+        # user_id = request.data.pop('user_id')[0]
         reviewimages = request.data.pop('image')
         
         # 리뷰 모델 저장
@@ -51,6 +53,17 @@ class ReviewView(APIView):
                 return Response(reviewimage_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
         return Response({"message": "저장 완료"}, status=status.HTTP_200_OK)
+
+
+# 리뷰 작성 페이지에서 user trip 받아오기
+class ReviewWriteView(APIView):
+    def get(self, request):
+        # user = request.user.id
+        user = 1
+        user_trips = Trip.objects.filter(user=user)
+        trip_serializer = ReviewUserTripSerializer(user_trips, many=True).data
+
+        return Response({"trip_list":trip_serializer}, status=status.HTTP_200_OK)
 
 
 # 리뷰 상세 기능
