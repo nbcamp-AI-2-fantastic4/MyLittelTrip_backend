37fe0065591acc668712badf6cf1733eb248bdc6
mankic <miic0803@gmail.com>
2022-08-03 23:51:22 +0900

    수정 - CORS 설정 추가

--- MyLittleTrip_backend/settings.py
+++ MyLittleTrip_backend/settings.py
@@ -42,15 +42,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'user',
     'place',
     'recommend',
     'trip',
     'comment',
-    'review',    
+    'review',
+    'corsheaders',    
 ]
 
 MIDDLEWARE = [
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'MyLittleTrip_backend.urls'
@@ -154,6 +158,7 @@ REST_FRAMEWORK = {
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 # JWT 설정
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
@@ -187,3 +192,14 @@ SIMPLE_JWT = {
     'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
+
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+]
+
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:5500'
+                         ,'http://localhost:5500']
+
+CORS_ALLOW_CREDENTIALS = True
