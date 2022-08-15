4d0a33e875d32eaafda63f692c0b9f82d8ec6f8d
mankic <miic0803@gmail.com>
2022-08-15 15:04:52 +0900

    수정 - CORS 수정

--- MyLittleTrip_backend/settings.py
+++ MyLittleTrip_backend/settings.py
@@ -53,11 +53,11 @@ INSTALLED_APPS = [
     'trip',
     'comment',
     'review',
-    # 'corsheaders',    
+    'corsheaders',    
 ]
 
 MIDDLEWARE = [
-    # 'corsheaders.middleware.CorsMiddleware', 
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -65,7 +65,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'MyLittleTrip_backend.urls'
@@ -202,12 +201,14 @@ SIMPLE_JWT = {
 }
 
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5500",
-#     "http://127.0.0.1:5500",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "http://0.0.0.0:0",
+]
 
-# CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:5500'
-#                          ,'http://localhost:5500']
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:5500'
+                         ,'http://localhost:5500'
+                         , 'http://0.0.0.0:0']
 
-# CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
